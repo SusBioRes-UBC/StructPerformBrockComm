dcc417d7fe329f79c036109125acccdf5a465f84
Qingshi Tu <zerateltu85@gmail.com>
2021-07-13 17:15:59 -0700

    July 13 update

--- brock_comm_CLT_perform.py
+++ brock_comm_CLT_perform.py
@@ -1,7 +1,7 @@
 """
 This script conducts the time-series analysis for the following target variables for UBC Brock Commons Tallwood House:
 	- moisture performance of CLT
-	- vertical movement
+	- vertical movement (??)
 
 Author: Qingshi
 
@@ -18,6 +18,8 @@ import pmdarima as pm
 import pandas as pd
 import brock_comm_config as config
 import os
+from datetime import datetime as dt
+import logging
 
 
 class CLT_perform:
@@ -25,7 +27,40 @@ class CLT_perform:
 	This class conducts the time-series analysis for a SINGLE floor
 	"""
 
-	def __init__(self, datasheets_path, floor_name):
+	def __init__(self, floor_file_name):
 		# load datasheet
-		sheet_path = os.path.sep.join([datasheets_path, floor_name])
-		self.worksheet = pd.read_csv(sheet_path)
+		sheet_path = os.path.sep.join([config.DATASHEETS_PATH, floor_file_name])
+		self.worksheet = pd.read_csv(sheet_path, index_col=False)
+
+		"""
+		=================
+		set up the logger
+		=================
+		"""
+		# gets or creates a logger
+		self.logger = logging.getLogger(__name__)  
+
+		# set log level
+		self.logger.setLevel(logging.INFO)
+
+		# define file handler and set formatter
+		file_handler = logging.FileHandler('log_LCA_calculation.log')
+		formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+		file_handler.setFormatter(formatter)
+
+		# add file handler to logger
+		self.logger.addHandler(file_handler)  
+
+
+	def preprocess(self):
+		# data cleaning
+		# - remove white space for column names
+		self.worksheet.columns = [x.strip() for x in list(self.worksheet.columns)]
+		# - create two new columns to store Date and Time separately
+		self.worksheet['Date'] = self.worksheet["DateTime"].apply(lambda x: dt.strptime(x, "%Y-%m-%d %H:%M:%S%z").date())
+		self.worksheet['Time'] = self.worksheet["DateTime"].apply(lambda x: dt.strptime(x, "%Y-%m-%d %H:%M:%S%z").time())
+
+		# 
+
+
+
