fa49b1b3dc88cbcc35046a3387b3f2ba9cd0400c
Qingshi Tu <zerateltu85@gmail.com>
2021-07-15 20:09:35 -0700

    updated data cleaning procedure

--- brock_comm_CLT_perform.py
+++ brock_comm_CLT_perform.py
@@ -1,12 +1,13 @@
 """
 This script conducts the time-series analysis for the following target variables for UBC Brock Commons Tallwood House:
 	- moisture performance of CLT
-	- vertical movement (??)
+	- vertical movement
 
 Author: Qingshi
 
 References:
-- UBC Brock Commons Structural Performance Report Sept 2020: https://sustain.ubc.ca/sites/default/files/UBC%20Brock%20Commons%20Structural%20Performance%20Report%20Sept%202020.pdf 
+- UBC Brock Commons Structural Performance Report Sept 2020: https://sustain.ubc.ca/sites/default/files/UBC%20Brock%20Commons%20Structural%20Performance%20Report%20Sept%202020.pdf
+- CCBST 2017 Moisture Performance and Vertical Movement Monitoring of Pre-Fabricated CLT - Paper No 88 
 """
 
 """
@@ -20,16 +21,17 @@ import brock_comm_config as config
 import os
 from datetime import datetime as dt
 import logging
+from copy import deepcopy
 
 
 class CLT_perform:
 	"""
-	This class conducts the time-series analysis for a SINGLE floor
+	This class conducts the time-series analysis for a SINGLE csv file
 	"""
 
-	def __init__(self, floor_file_name):
+	def __init__(self, csv_file_name):
 		# load datasheet
-		sheet_path = os.path.sep.join([config.DATASHEETS_PATH, floor_file_name])
+		sheet_path = os.path.sep.join([config.DATASHEETS_PATH, csv_file_name])
 		self.worksheet = pd.read_csv(sheet_path, index_col=False)
 
 		"""
@@ -44,23 +46,45 @@ class CLT_perform:
 		self.logger.setLevel(logging.INFO)
 
 		# define file handler and set formatter
-		file_handler = logging.FileHandler('log_LCA_calculation.log')
+		file_handler = logging.FileHandler('CLT_perform.log')
 		formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
 		file_handler.setFormatter(formatter)
 
 		# add file handler to logger
 		self.logger.addHandler(file_handler)  
 
-
-	def preprocess(self):
-		# data cleaning
+		"""
+		=============
+		data cleaning
+		=============
+		"""
 		# - remove white space for column names
 		self.worksheet.columns = [x.strip() for x in list(self.worksheet.columns)]
+		# - remove white space for str objects in data columns & replace 'NULL' with 'None'
+		for col_name in self.worksheet.columns:
+			self.worksheet[col_name] = self.worksheet[col_name].apply(lambda x: x.strip() if isinstance(x, str) else x)
+			self.worksheet[col_name] = self.worksheet[col_name].apply(lambda x: None if x == 'NULL' else x)
 		# - create two new columns to store Date and Time separately
 		self.worksheet['Date'] = self.worksheet["DateTime"].apply(lambda x: dt.strptime(x, "%Y-%m-%d %H:%M:%S%z").date())
 		self.worksheet['Time'] = self.worksheet["DateTime"].apply(lambda x: dt.strptime(x, "%Y-%m-%d %H:%M:%S%z").time())
+		# - get a list of data column names 
+		self.data_columns = [col_name for col_name in self.worksheet.columns if col_name not in ['Date', 'Time', 'DateTime']]
 
-		# 
+	def preprocess(self, col_name):
+		"""
+		this method does the following for ONE column of a dataframe:
+			- log the information of data
+		"""
+		# get a list of timestamps where there is missing data
+		boolean_mask= pd.isnull(self.worksheet[col_name])
+		missing_data_timestamps = list(self.worksheet['DateTime'][boolean_mask])
+
+		# log the information of the data
+		self.logger.info(f"==== statistics of {col_name} column ====")
+		self.logger.info(f"timestamps of missing data are {missing_data_timestamps}")
+		self.logger.info(" ")
+
+		# retain the time series data with
 
 
 
