9ccd35b9c07374aa6c725c2c18ed6673798913c2
Qingshi Tu <zerateltu85@gmail.com>
2021-08-04 22:09:48 -0700

    created regressor helper function

--- regressor_helper.py
+++ regressor_helper.py
@@ -1,3 +1,24 @@
 """
 This helper function prepares the data of regressor(s) to match the format required by FB Prophet
 """
+
+"""
+================
+Import libraries
+================
+"""
+import pandas as pd
+
+
+class RegressHelp:
+
+	def prepare_climate_regr(self,raw_clmate_data, **kwargs):
+		# load climate data
+		self.raw_climate_data = pd.read_csv(raw_clmate_data, parse_dates=['LOCAL_DATE'])
+		self.retained_climate_data = self.raw_climate_data[['LOCAL_DATE', 'MEAN_TEMPERATURE','TOTAL_PRECIPITATION']]
+
+		# check if need to expand daily data to hourly 
+		if 'convert_day_to_hour_interval' in kwargs:
+			# use pd.resample to expand daily data to hourly data
+			self.retained_climate_data.set_index('LOCAL_DATE', inplace=True)
+			self.retained_climate_data = self.retained_climate_data.resample(kwargs['convert_day_to_hour_interval']).pad()
