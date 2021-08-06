b09513221df6337f792c91c72ce7bbd6de3a1bb7
Qingshi Tu <zerateltu85@gmail.com>
2021-08-06 16:43:18 -0700

    initiated (currently placeholder) a method to match the timeseries data and regressor data

--- regressor_helper.py
+++ regressor_helper.py
@@ -14,11 +14,32 @@ class RegressHelp:
 
 	def prepare_climate_regr(self,raw_clmate_data, **kwargs):
 		# load climate data
-		self.raw_climate_data = pd.read_csv(raw_clmate_data, parse_dates=['LOCAL_DATE'])
-		self.retained_climate_data = self.raw_climate_data[['LOCAL_DATE', 'MEAN_TEMPERATURE','TOTAL_PRECIPITATION']]
+		raw_climate_data = pd.read_csv(raw_clmate_data, parse_dates=['LOCAL_DATE'])
+		retained_climate_data = raw_climate_data[['LOCAL_DATE', 'MEAN_TEMPERATURE','TOTAL_PRECIPITATION']]
 
 		# check if need to expand daily data to hourly 
 		if 'convert_day_to_hour_interval' in kwargs:
 			# use pd.resample to expand daily data to hourly data
-			self.retained_climate_data.set_index('LOCAL_DATE', inplace=True)
-			self.retained_climate_data = self.retained_climate_data.resample(kwargs['convert_day_to_hour_interval']).pad()
+			retained_climate_data.set_index('LOCAL_DATE', inplace=True)
+			retained_climate_data = self.retained_climate_data.resample(kwargs['convert_day_to_hour_interval']).pad()
+
+		return retained_climate_data
+
+	def matching_regr_data(self, regr_data, ts_data):
+		"""
+		matches the timestamp between regressor data and time series data
+		Arguments
+			- regr_data: a dataframe containing timestep and regressor data
+			- ts_data: a dataframe containing timestep and time series data
+		Returns:
+			- matched_regr_data: part of the regressor data with the common range of timestep 
+			- matched_ts_data: part of the time series data with the common range of timestep 
+		"""
+
+		# find the interection of timestep between two dataframes
+
+		# slice the two dataframe with the common timestamp range
+
+		# return two adjusted dataframes
+
+		pass
