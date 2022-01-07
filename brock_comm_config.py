092e5b8a6c2d8d47b64b97618c3b04932ff9f66c
Qingshi Tu <zerateltu85@gmail.com>
2022-01-06 19:23:53 -0800

    rerun the script to get back to speed

--- brock_comm_config.py
+++ brock_comm_config.py
@@ -3,7 +3,7 @@ This config file stores
 """
 
 # path to datasheets
-DATASHEETS_PATH = '/Users/Qingshi/Documents/StructPerformBrockComm/SMT 03.10.2020 TALLWOOD DATA EXPORT_July 2016 to March 2020 '
+DATASHEETS_PATH = '/Users/Qingshi/Documents/StructPerformBrockComm/TALLWOOD DATA'
 CLIMATE_DATA_PATH = '/Users/Qingshi/Documents/StructPerformBrockComm/climate_data'
 
 # path to output files
