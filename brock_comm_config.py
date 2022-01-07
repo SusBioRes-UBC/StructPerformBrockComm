004ec288727f08bcf9d8bb6be55f11ae7b238e3b
Tu <qingshi.tu@ubc.ca>
2022-01-06 18:37:32 -0800

    add path for Windows OS

--- brock_comm_config.py
+++ brock_comm_config.py
@@ -3,9 +3,12 @@ This config file stores
 """
 
 # path to datasheets
-DATASHEETS_PATH = '/Users/Qingshi/Documents/StructPerformBrockComm/SMT 03.10.2020 TALLWOOD DATA EXPORT_July 2016 to March 2020 '
-CLIMATE_DATA_PATH = '/Users/Qingshi/Documents/StructPerformBrockComm/climate_data'
+DATASHEETS_PATH = r"C:\Users\qtu2020\Documents\StructPerformBrockComm\TALLWOOD DATA"
+	#'/Users/Qingshi/Documents/StructPerformBrockComm/SMT 03.10.2020 TALLWOOD DATA EXPORT_July 2016 to March 2020 '
+CLIMATE_DATA_PATH = r"C:\Users\qtu2020\Documents\StructPerformBrockComm\climate_data"
+	#'/Users/Qingshi/Documents/StructPerformBrockComm/climate_data'
 
 # path to output files
-OUTPUT_PATH = '/Users/Qingshi/Documents/StructPerformBrockComm/output'
+OUTPUT_PATH = r"C:\Users\qtu2020\Documents\StructPerformBrockComm\output"
+	#'/Users/Qingshi/Documents/StructPerformBrockComm/output'
 
