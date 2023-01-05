a7312d09f3ee16ea46ca47157593d02bffbb5697
sbr <qingshi.tu@ubc.ca>
2023-01-05 12:59:48 -0800

    created a script for testing the prediction for CLT drying period only

---
+++ Prophet/test_predict_drying_period.py
@@ -0,0 +1,23 @@
+"""
+This script aims to test the code pipeline for predicting the CLT mositure lvl for drying period only
+"""
+
+
+"""
+================
+Import libraries
+================
+"""
+# import the pipelines 
+from Prophet.prediction import Prophet_Pipeline
+from Results_Analysis import Results_Analysis
+
+import pandas a pd
+
+
+"""
+======
+Script
+======
+"""
+
